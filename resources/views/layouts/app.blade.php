bc48a4261d068ef386917d437b11931d3458565a
Ebenaruto <omartial30@gmail.com>
2020-07-02 18:01:37 +0000

    authentification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,8 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="../assets/js/bootstrap.js"></script>
+    <script src="assets/js/materialize.min.js"></script>
+    <script src="assets/js/test.js"></script>
 
 
     <!-- Fonts -->
@@ -22,6 +24,8 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/materialize.min.css">
+    <link rel="stylesheet" href="assets/css/styl.css">
 
 </head>
 
